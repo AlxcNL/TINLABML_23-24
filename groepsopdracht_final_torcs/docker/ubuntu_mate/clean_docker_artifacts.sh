53b26b2edeea4c046311d54c5b1a4958fd8650c5
git_config.sh <AlxcNL>
2024-04-27 09:57:54 +0200

    Added clean script

---
+++ groepsopdracht_final_torcs/docker/ubuntu_mate/clean_docker_artifacts.sh
@@ -0,0 +1,9 @@
+  #!/usr/bin/env bash
+  
+docker container prune
+docker ps -a -q | xargs docker rm
+docker images -a -q | xargs docker rmi -f
+docker image prune
+docker image prune -a
+docker volume prune
+docker system prune
