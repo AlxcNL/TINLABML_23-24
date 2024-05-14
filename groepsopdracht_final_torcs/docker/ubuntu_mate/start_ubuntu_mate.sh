80b7a6bee5a4d31e559f04a95994b049abc3a6aa
git_config.sh <AlxcNL>
2024-05-14 20:12:47 +0200

    Minor change

--- groepsopdracht_final_torcs/docker/ubuntu_mate/start_ubuntu_mate.sh
+++ groepsopdracht_final_torcs/docker/ubuntu_mate/start_ubuntu_mate.sh
@@ -3,16 +3,14 @@
 
 container="torcs-server"
 runtime="docker"
-#tag="ubuntu-mate"
 tag="0.1"
 image="docker.io/jaboo/torcs-server"
-#image="lscr.io/linuxserver/webtop"
 host_port=3000
-host_port_https=3001
-container_port=3020
+host_port_https=3021
+container_port=3000
 container_port_https=3021
 
-./destroy_ubuntu_mate.sh || true
+./stop_ubuntu_mate.sh || true
 
 $runtime container run -id \
   --name=$container \
@@ -20,14 +18,13 @@ $runtime container run -id \
   -e PGID=1000 \
   -e TZ=Europe/Amsterdam \
   -e TITLE="TINLAB Torcs Server" \
-  -e CUSTOM_PORT=$container_port \
   -e CUSTOM_HTTPS_PORT=$container_port_https \
   -p "${host_port}:${container_port}" \
   -p "${host_port_https}:${container_port_https}" \
   -v "${PWD}/config:/config" \
   -v "${PWD}/desktop:/config/Desktop" \
   -v "${PWD}/pics:/config/Pictures" \
-  "${image}:${tag}"
+  "${image}:${tag}" &
 
 sleep 4;
 python3 -m webbrowser http://localhost:${host_port}
