35c94e8d603cfe2e31b27e4f3d9e1485b4ec71f7
AlxcNL <J.A.Boogaard@hr.nl>
2024-05-14 16:17:20 +0200

    Changed port mapping

--- groepsopdracht_final_torcs/docker/ubuntu_mate/start_ubuntu_mate.sh
+++ groepsopdracht_final_torcs/docker/ubuntu_mate/start_ubuntu_mate.sh
@@ -3,23 +3,27 @@
 
 container="torcs-server"
 runtime="docker"
-tag="ubuntu-mate"
-image="torcs-server:0.1"
-host_port=3020
-host_port_https=3031
+#tag="ubuntu-mate"
+tag="0.1"
+image="docker.io/jaboo/torcs-server"
+#image="lscr.io/linuxserver/webtop"
+host_port=3000
+host_port_https=3001
+container_port=3020
+container_port_https=3021
 
-./destroy_ubuntu_mate.sh
+./destroy_ubuntu_mate.sh || true
 
-$runtime container run -d \
+$runtime container run -i \
   --name=$container \
   -e PUID=1000 \
   -e PGID=1000 \
   -e TZ=Europe/Amsterdam \
   -e TITLE="TINLAB Torcs Server" \
-  -p $host_port:3000 \
-  -p $host_port_https:3001 \
+  -p "$host_port:$container_port" \
+  -p "$host_port_https:$container_port_https" \
   -v "$PWD/config:/config" \
   "$image:$tag"
 
 sleep 4;
-python3 -m webbrowser http://localhost:${host_port}/
+python3 -m webbrowser http://localhost:${host_port}
