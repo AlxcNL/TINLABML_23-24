ffe896881638637bcb7900ad7fdd14d68b4344e7
AlxcNL <J.A.Boogaard@hr.nl>
2024-05-20 16:18:18 +0200

    Update from envy

--- python/install_requirements.sh
+++ python/install_requirements.sh
@@ -1,44 +1,64 @@
 #!/usr/bin/env bash
 
-echo "Config solver libmamba"
-conda install -y conda-libmamba-solver \
-&& conda config --set solver libmamba
+function install_requirements() {
+    echo "Config solver libmamba"
+    conda install -y conda-libmamba-solver \
+    && conda config --set solver libmamba
 
-echo "Update conda"
-conda update -c defaults conda \
+    echo "Update conda"
+    conda update -c defaults conda \
 
-echo "Install packages with conda"
-conda install -y -c conda-forge pytorch \
-    bash_kernel \
-    bpython \
-    conda-bash-completion \
-    ipykernel \
-    gymnasium \
-    joblib \
-    jupyterlab \
-    jupyter_console \
-    jupyterlab_widgets \
-    matplotlib \
-    nbconvert \
-    more-itertools \
-    numpy \
-    pandoc \
-    pandas \
-    pathlib \
-    pypdf2 \
-    scipy \
-    scikit-learn \
-    seaborn \
-    shellcheck \
-    subprocess32 \
-    cpuonly \
-    pytorch \
-    torchvision
+    echo "Install packages with conda"
+    conda install -y -c conda-forge pytorch \
+        bash_kernel \
+        bpython \
+        conda-bash-completion \
+        ipykernel \
+        gymnasium \
+        joblib \
+        jupyterlab \
+        jupyter_console \
+        jupyterlab_widgets \
+        matplotlib \
+        nbconvert \
+        more-itertools \
+        numpy \
+        pandoc \
+        pandas \
+        pathlib \
+        pypdf2 \
+        scipy \
+        scikit-learn \
+        seaborn \
+        shellcheck \
+        subprocess32 
 
-# conda install -y -c pytorch \
-#     cpuonly \
-#     pytorch \
-#     torchvision
+}
 
-pip install --upgrade pip \
-    tensorflow
+    # cpuonly \
+function install_tf() {
+    echo "Install TensorFlow with ROCM"
+    sudo apt-get install -y rocm-libs rccl
+    pip install --upgrade pip \
+        protobuf==3.19.0 \
+        tensorflow \
+        tensorflow-rocm
+
+    # echo "Test Tensorflow installation"
+
+}
+
+function install_pt() {
+    # https://rocm.docs.amd.com/projects/install-on-linux/en/latest/how-to/3rd-party/pytorch-install.html
+    echo "Install PyTorch with ROCM"
+    pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/rocm6.1/ && \
+    pytorch
+    
+    echo "Test PyTorch installation"
+    python -c 'import torch' 2> /dev/null && echo 'Success' || echo 'Failure'    
+
+}
+
+# install_requirements
+# install_tf
+install_pt
--- python/install_rocm.sh
+++ python/install_rocm.sh
@@ -1,41 +1,57 @@
 #!/usr/bin/env bash
 # https://rocm.docs.amd.com/projects/install-on-linux/en/latest/how-to/native-install/ubuntu.html
 
-# Make the directory if it doesn't exist yet.
-# This location is recommended by the distribution maintainers.
-sudo mkdir --parents --mode=0755 /etc/nala/keyrings
+# # Make the directory if it doesn't exist yet.
+# # This location is recommended by the distribution maintainers.
+# sudo mkdir --parents --mode=0755 /etc/nala/keyrings
 
-# Download the key, convert the signing-key to a full
-# keyring required by nala and store in the keyring directory
-wget https://repo.radeon.com/rocm/rocm.gpg.key -O - | \
-    gpg --dearmor | sudo tee /etc/nala/keyrings/rocm.gpg > /dev/null
+# # Download the key, convert the signing-key to a full
+# # keyring required by nala and store in the keyring directory
+# wget https://repo.radeon.com/rocm/rocm.gpg.key -O - | \
+#     gpg --dearmor | sudo tee /etc/nala/keyrings/rocm.gpg > /dev/null
 
-echo "deb [arch=amd64 signed-by=/etc/nala/keyrings/rocm.gpg] https://repo.radeon.com/amdgpu/6.1.1/ubuntu jammy main" \
-    | sudo tee /etc/nala/sources.list.d/amdgpu.list
-sudo nala update    
+# echo "deb [arch=amd64 signed-by=/etc/nala/keyrings/rocm.gpg] https://repo.radeon.com/amdgpu/6.1.1/ubuntu jammy main" \
+#     | sudo tee /etc/nala/sources.list.d/amdgpu.list
 
-echo "deb [arch=amd64 signed-by=/etc/nala/keyrings/rocm.gpg] https://repo.radeon.com/rocm/nala/6.1.1 jammy main" \
-    | sudo tee --append /etc/nala/sources.list.d/rocm.list
-echo -e 'Package: *\nPin: release o=repo.radeon.com\nPin-Priority: 600' \
-    | sudo tee /etc/nala/preferences.d/rocm-pin-600
-sudo nala update
+# sudo nala update    
 
-sudo nala install -y amdgpu-dkms \
-    rocm
+# echo "deb [arch=amd64 signed-by=/etc/nala/keyrings/rocm.gpg] https://repo.radeon.com/rocm/nala/6.1.1 jammy main" \
+#     | sudo tee --append /etc/nala/sources.list.d/rocm.list
 
-# Post-installation instructions
+# echo -e 'Package: *\nPin: release o=repo.radeon.com\nPin-Priority: 600' \
+#     | sudo tee /etc/nala/preferences.d/rocm-pin-600
 
-sudo tee --append /etc/ld.so.conf.d/rocm.conf <<EOF
-/opt/rocm/lib
-/opt/rocm/lib64
-EOF
-sudo ldconfig
+# sudo nala update && \
+# sudo nala install -y amdgpu-dkms \
+#     rocm
 
-export PATH=$PATH:/opt/rocm-6.1.1/bin
+# # Post-installation instructions
 
-dkms status
+# sudo tee --append /etc/ld.so.conf.d/rocm.conf <<EOF
+# /opt/rocm/lib
+# /opt/rocm/lib64
+# EOF
 
-/opt/rocm-6.1.1/bin/rocminfo
-/opt/rocm-6.1.1/bin/clinfo
+# sudo ldconfig
 
-echo "Reboot your system"
+# export PATH=$PATH:/opt/rocm-6.1.1/bin
+
+# dkms status
+
+# /opt/rocm-6.1.1/bin/rocminfo
+# /opt/rocm-6.1.1/bin/clinfo
+
+# echo "Reboot your system"
+
+
+# Installation via AMDGPU installer
+
+# https://rocm.docs.amd.com/projects/install-on-linux/en/latest/how-to/amdgpu-install.html
+# https://rocm.docs.amd.com/projects/install-on-linux/en/latest/how-to/prerequisites.html
+
+sudo apt install "linux-headers-$(uname -r)" "linux-modules-extra-$(uname -r)"
+
+echo "Setting permissions for groups"
+echo 'ADD_EXTRA_GROUPS=1' | sudo tee -a /etc/adduser.conf
+echo 'EXTRA_GROUPS=video' | sudo tee -a /etc/adduser.conf
+echo 'EXTRA_GROUPS=render' | sudo tee -a /etc/adduser.conf
