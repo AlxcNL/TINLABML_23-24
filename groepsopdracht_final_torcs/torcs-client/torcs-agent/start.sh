a53f3669b24ba0e795cf996442bafdfc2677c807
AlxcNL <J.A.Boogaard@hr.nl>
2024-05-24 15:44:14 +0200

    Update after lesson

--- groepsopdracht_final_torcs/torcs-client/torcs-agent/start.sh
+++ groepsopdracht_final_torcs/torcs-client/torcs-agent/start.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+hostname="localhost"
+
 if [[ -z "$1" ]]; then
     echo "Error. $0 requires a portnumber"
     exit 1;
@@ -7,5 +9,7 @@ fi
 
 portnr="$1"
 
-echo "Torcs client is waiting for a connection with torcs-server at $portnr"
-python ./run.py --port $portnr
+[[ -n "$2" ]] && hostname="$2"
+
+echo "Torcs client is waiting for a connection with $hostname torcs-server at $portnr"
+python ./run.py --port $portnr --hostname $hostname 
