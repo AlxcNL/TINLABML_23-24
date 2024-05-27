b86cd878f6d2b3eab8055b9ff148e582efda1a83
git_config.sh <AlxcNL>
2024-05-27 23:51:42 +0200

    Removed torcs-client installation

--- groepsopdracht_final_torcs/docker/ubuntu_mate/create_ubuntu_mate.sh
+++ groepsopdracht_final_torcs/docker/ubuntu_mate/create_ubuntu_mate.sh
@@ -31,10 +31,5 @@ $runtime container run -id \
 echo "Create Desktop shortcut"
 docker cp desktop/torcs.desktop $container:/config/Desktop;
 docker cp pics/Torcs-title.png $container:/config/Pictures;
-docker cp install/scripts/install_torcs_client.sh $container:/config/;
-
-echo "Install Torcs Client example"
-docker container exec -i $container chmod u+x/config/install_torcs_client.sh
-docker container exec -i $container /config/install_torcs_client.sh
 
 ./stop_ubuntu_mate.sh
