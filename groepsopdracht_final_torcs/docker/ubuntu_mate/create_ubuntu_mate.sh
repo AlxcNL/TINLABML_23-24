09c39678f2516414ac2886fb8f66e4581614c1ed
git_config.sh <AlxcNL>
2024-05-28 21:58:08 +0200

    Refactoring

--- groepsopdracht_final_torcs/docker/ubuntu_mate/create_ubuntu_mate.sh
+++ groepsopdracht_final_torcs/docker/ubuntu_mate/create_ubuntu_mate.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 # Author: J.A.Boogaard@hr.nl
 
-container="torcs-server"
 runtime="docker"
-tag="0.2"
-image="jaboo/torcs-server"
+
+repo="jaboo"
+version="0.2"
+arch=$(uname -m)
+image_name="torcs-server"
+tag="${version}-${arch}"
+image="${repo}/${image_name}:${tag}"
+
+container="torcs-server"
+
 host_port=3000
 host_port_https=3021
 container_port=3000
@@ -15,22 +22,15 @@ container_port_https=3021
 mkdir -p config
 mkdir -p logs
 
-$runtime container run -id \
-  --name=$container \
-  -e PUID=1000 \
-  -e PGID=1000 \
-  -e TZ=Europe/Amsterdam \
-  -e TITLE="TINLAB Torcs Server" \
-  -e CUSTOM_HTTPS_PORT=$container_port_https \
-  -p "${host_port}:${container_port}" \
-  -p "${host_port_https}:${container_port_https}" \
-  -v "${PWD}/config:/logs" \
-  -v "${PWD}/config:/config" \
-  "${image}:${tag}"
-
-echo "Create Desktop shortcut"
-docker cp desktop/mate-terminal.desktop $container:/config/Desktop/;
-docker cp desktop/torcs.desktop $container:/config/Desktop/;
-docker cp pics/Torcs-title.png $container:/config/Pictures/;
+cmd="$runtime container run -id --name=$container"
+cmd="${cmd} -e PUID=1000 -e PGID=1000 -e TZ=Europe/Amsterdam -e TITLE=\"TINLAB Torcs Server\""
+cmd="${cmd} -e CUSTOM_HTTPS_PORT=$container_port_https -p ${host_port}:${container_port}"
+cmd="${cmd} -p ${host_port_https}:${container_port_https}"
+cmd="${cmd} -v ${PWD}/config:/config ${image}"
+cmd="${cmd} -v ${PWD}/desktop:/config/Desktop"
+cmd="${cmd} -v ${PWD}/pics:/config/Pictures/"
+cmd="${cmd} -v ${PWD}/config:/logs"
+echo $cmd
+eval $cmd
 
 ./stop_ubuntu_mate.sh
