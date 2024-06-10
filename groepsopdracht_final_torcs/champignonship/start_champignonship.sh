e575ec2e19a7a439240bbe7410289357953f4f24
AlxcNL <J.A.Boogaard@hr.nl>
2024-06-10 19:30:56 +0200

    Start and stop with docker-compose

--- groepsopdracht_final_torcs/champignonship/start_champignonship.sh
+++ groepsopdracht_final_torcs/champignonship/start_champignonship.sh
@@ -1,14 +1,6 @@
 #!/usr/bin/env bash
 # Author: J.A.Boogaard@hr.nl
 
-repo="jaboo"
-image_name="torcs-agent"
-tag="0.1"
-image="${repo}/${image_name}:${tag}"
-entrypoint="bash"
-container="torcs-server"
-
-cmd="docker run --add-host host.docker.internal:host-gateway"
-cmd="${cmd} --rm -it --name $container $image"
+cmd="docker-compose -f champignonship.yaml up"
 echo $cmd
 eval $cmd
