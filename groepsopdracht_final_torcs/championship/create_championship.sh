3f20abd890687ec47f0688c21fe184fe42b57ab9
AlxcNL <J.A.Boogaard@hr.nl>
2024-06-27 16:33:34 +0200

    Flexible way to create championship

--- groepsopdracht_final_torcs/championship/create_championship.sh
+++ groepsopdracht_final_torcs/championship/create_championship.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 header="services:"
-compose_file="../championship.yaml"
+compose_file="championship.yaml"
+teams_dir="teams"
 
-(   
-    cd teams;
-    printf $header > $compose_file;
-    cmd="ls | grep -e \"[0-9][0-9]\" | grep -vE 00";
-    cmd="${cmd} | xargs -I {} cat {}/docker-compose.yaml";
-    cmd="${cmd} | sed 's/$header/\\n/'  >> ${compose_file}";
-    echo $cmd;
-    eval $cmd;
+printf "%s\n" $header > $compose_file;
 
-)
+
+for team in "$@"
+do
+    [[ -n $1 ]] && team=$1
+    [[ ${#team} == 1 ]] && team="0${team}"
+    cat "${teams_dir}/${team}/docker-compose.yaml" | sed 's/services://' >> $compose_file    
+    echo >> $compose_file
+
+done
